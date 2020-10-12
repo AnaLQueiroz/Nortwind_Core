98b39795357007d7036cc6006131d0904f5bcbc0
Ana Paula Queiroz <aplqueiroz86@gmail.com>
2020-10-11 22:45:27 -0300

    Add project files.

---
+++ Nortwind/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nortwind.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Nortwind
+
+Projeto de Estudo seguindo a video-aula do Prof. Marcoratti
+Tema: ASP .NET Core 3.1 com EF Core - DataBase First
+Youtube: https://www.youtube.com/watch?v=Y22WGhUI2rs
